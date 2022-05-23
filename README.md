1d383e933ffbf0b2d2bada50dfb28050954c2ddb
j_park <>
2022-05-23 16:52:46 +0900

    製作中

--- README.md
+++ README.md
@@ -9,3 +9,16 @@ https://script.google.com/home/projects/1UDGucuwfUK2_jRdh9-6WvyuEte73oM1qOvPFo2Y
 **관련 시트**
 https://docs.google.com/spreadsheets/d/1x-OR7nX4vt2pyUKpYhQCnB3SSnF2mRybr-z0ayglY6w/edit#gid=0
 
+**명령어 정리**
+- 가상 환경 만들기
+  `python3 -m venv 가상 환경의 이름`
+- 설치된 패키지 확인
+  `pip freeze`
+- 설치된 패키지를 관리하는 requirements.txt 파일 만들기
+  `pip freeze > requirements.txt`
+- requirements.txt 에 적혀있는 패키지 설치하기
+  `pip install -r requirements.txt`
+- requirements.txt 에 적혀있는 패키지 삭제하기
+  `pip uninstall -r requirements.txt -y`
+- 가상환경 터미널에서 활성화시키기
+  `source pTest_venv/bin/activate`
--- TEST.py
+++ TEST.py
@@ -1,6 +1,8 @@
 # 다른 파일의 함수를 사용하기 : import해서 사용한다.
-from resources import sendMail
-from resources import canConnectWeb
+import schedule
+import time
+# from resources import sendMail
+# from resources import canConnectWeb
 
 url = ""
 
@@ -25,6 +27,21 @@ groupData = {
     url2: [mail3]
 }
 
-print(groupData)
-print(groupData[url2])
-# print(groupData["https://jidae.com"])
+# https://wikidocs.net/137924
+
+# step1.관련 패키지 및 모듈 import
+
+# step2.실행할 함수 선언
+def message():
+    print(groupData)
+    print(groupData[url2])
+    # print(groupData["https://jidae.com"])
+
+# step3.실행 주기 설정
+schedule.every(60).minutes.do(message)
+# schedule.every(3).seconds.do(message)
+
+# step4.스캐쥴 시작
+while True:
+    schedule.run_pending()
+    time.sleep(1)
