a66b0402edb376923810ab762168559c38bc4f07
j_park <>
2022-05-25 17:32:54 +0900

    메일 발송 함수 정리

--- resources/sendMail.py
+++ resources/sendMail.py
@@ -1,20 +1,18 @@
 # https://docs.python.org/3.9/library/email.examples.html
 # https://yeolco.tistory.com/93
 
-def sendMail():
+def sendMail(mailContent, mailSubject, mailSender, mailReceiverBcc):
     import smtplib
     from email.mime.text import MIMEText
     from email.message import EmailMessage
-    import time
 
     # 보낼 메시지 설정
     msg = EmailMessage()
-    now = time.strftime('%Y-%m-%d %H:%M:%S')
-    msg.set_content(f'내용 : 본문내용 테스트입니다. : {now}')
-    msg['Subject'] = '제목 : 메일 보내기 테스트입니다.'
-    msg['From'] = 'createzonebot@gmail.com'
+    msg.set_content(mailContent)
+    msg['Subject'] = mailSubject
+    msg['From'] = mailSender
     msg['To'] = ''
-    msg['Bcc'] = 'createzone+TEST@gmail.com, createzone+TEST2@gmail.com'
+    msg['Bcc'] = mailReceiverBcc
 
     # print(msg.keys())
 
@@ -26,4 +24,13 @@ def sendMail():
     mailSession.quit() # 세션 종료
 
 if __name__=="__main__":
-    sendMail()
+    import time
+
+    now = time.strftime('%Y-%m-%d %H:%M:%S')
+
+    mailContent = f'ruliweb.com\n의 접속 상태가 「접속 가능」에서 「접속 불가능」으로 바뀌었습니다.\n의 접속 상태가 「접속 불가능」에서 「접속 가능」으로 바뀌었습니다.\n\n변경 확인 시간 : {now}'
+    mailSubject = "`ruliweb.com` 에 접속 가능 여부에 변경이 있었습니다."
+    mailSender = 'createzonebot@gmail.com'
+    mailReceiverBcc = 'createzone+TEST@gmail.com, createzone+TEST2@gmail.com'
+    
+    sendMail(mailContent, mailSubject, mailSender, mailReceiverBcc)
